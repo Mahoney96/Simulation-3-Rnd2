a31b5e34ecd54dcb62a46b4c50a21c42dd1dd84d
Mahoney96 <cmahoney966@gmail.com>
2020-08-16 03:27:08 -0700

    further set up

---
+++ src/Components/DashBoard/DashBoard.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const DashBoard = () => {
+    return <div>
+        This is the DashBoard Component!!
+    </div>
+};
+
+export default DashBoard;
---
+++ src/Components/Post/Post.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Post = () => {
+    return <div>
+        This is the Post the Component!!
+    </div>
+}
+
+export default Post;
