3ceb28b5ab64f336b4db261b486f541c196ce4fa
Mahoney96 <cmahoney966@gmail.com>
2020-08-17 14:40:32 -0700

    restarting another version of simulation to start fresh again

---
+++ routing_practice/app2.js
@@ -0,0 +1,21 @@
+imort React, {Component } from "react";
+imort "./style.css";
+import routes from './routes';
+import { Component } from 'react';
+import {withRouting} from 'react-router-dom';
+
+export default class app extends Component {
+    constructor
+}
+
+class App extends Component {
+    constructor(props) {
+        super(props):
+        this.state = {
+            loggedIn: false
+        }
+    }
+
+
+
+}
